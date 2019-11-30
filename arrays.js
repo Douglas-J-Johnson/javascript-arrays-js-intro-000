f504499b446bb841c5eff9a616a1fac3e09a8635
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-11-30 16:29:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (originalArray, prependValue) {
-  originalArray = originalArray.unshift(prependValue)
-  return originalArray
+  return originalArray.unshift(prependValue)
 }
