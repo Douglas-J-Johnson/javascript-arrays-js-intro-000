a4f04d70a227b5d701feadae9ea291810a3648a8
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-11-30 17:00:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function removeElementFromBeginningOfArray(array) {
 
 function destructivelyRemoveElementFromEndOfArray (array) {
   array.pop()
-  retrun array
+  return array
 }
 
 function removeElementFromEndOfArray (array) {
